7cd9fa306761dd44cfd6f5068fbbf1a010fc3925
kcbar <Kbarter@mail.greenriver.edu>
2019-07-24 17:41:32 -0700

    Learning React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Projects from './Projects';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    state = { displayBio: false };
+
+    // constructor(){
+    //     super();
+    //     this.state = { displayBio: false };
+
+    //     this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    // }
+
+    toggleDisplayBio = () => {
+        this.setState({ displayBio: !this.state.displayBio })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Hello</h1>
+                <p>My name is Kevan. I'm a software engineer</p>
+                <p>I'm always looking foward to working on meaningful projects</p>
+                {
+                    this.state.displayBio ? (<div>
+                        <p>I live in Seattle, and code every day.</p>
+                        <p>I enjoy all aspects of Software Development</p>
+                        <button onClick={this.toggleDisplayBio}>Show less</button>
+                    </div>) : (
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read more</button>
+                        </div>
+                    )
+                }
+                <hr/>
+                <Projects />
+            </div>
+        )
+    }
 }
 
 export default App;
